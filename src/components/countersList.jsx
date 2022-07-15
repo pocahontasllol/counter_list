dfaf9eda972c5e1f09f9b339f8fc764955a150f0
pocahontasllol <pocahontasllol@gmail.com>
2022-07-15 17:35:41 +0300

    buttons

--- src/components/countersList.jsx
+++ src/components/countersList.jsx
@@ -2,28 +2,51 @@ import React, { useState } from "react";
 import Counter from "./counter";
 
 const CountersList = () => {
-  const [counters, setCounters] = useState([
-    { id: 0, value: 1, name: "Ненужная вещь" },
+  const initialState = [
+    { id: 0, value: 1, name: "Ненужная вещь", price: "200" },
     { id: 1, value: 4, name: "Ложка" },
     { id: 2, value: 3, name: "Вилка" },
     { id: 3, value: 2, name: "Тарелка" },
     { id: 4, value: 1, name: "Набор минималиста" },
-  ]);
+  ];
+  const [counters, setCounters] = useState(initialState);
   const handleDelete = (id) => {
     const newCounters = counters.filter((c) => c.id !== id);
     setCounters(newCounters);
   };
+  const handleIncrement = (id) => {
+    const plus = counters.map((item) => {
+      if (item.id === id) {
+        item.value += 1;
+      }
+      return item;
+    });
+    setCounters(plus);
+  };
+  const handleDecrement = (id) => {
+    const minus = counters.map((item) => {
+      if (item.id === id) {
+        item.value -= 1;
+      }
+      return item;
+    });
+    setCounters(minus);
+  };
+  const handleReset = () =>{
+    setCounters(initialState)
+  }
   return (
     <>
       {counters.map((count) => (
         <Counter
           key={count.id}
-          id={count.id}
-          value={count.value}
-          name={count.name}
           onDelete={handleDelete}
+          onIncrement={handleIncrement}
+          onDecrement={handleDecrement}
+          {...count}
         />
       ))}
+      <button className="btn btn-primary btn-sm m-2" onClick={handleReset}>сброс</button>
     </>
   );
 };
